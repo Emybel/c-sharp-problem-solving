5c570c1f493852ba0712f7b84ceeb1440d9773c6
emybel <belaidimane@gmail.com>
2024-06-08 09:09:06 +0100

    updates

--- set-1/challenge_25.cs
+++ set-1/challenge_25.cs
@@ -1,21 +1,27 @@
 class Challenge25{
-public static String NumberOfCopies(){
+    public static String NumberOfCopies(){
 
-    int n = 4;
-    String str = "JS";
-    //Repeat the string to the number of n
-    
-    // This is the short solution by using (Concat and Repeat methods)
-    // String result = string.Concat(Enumerable.Repeat(str, n));
-    
-    // This is the longer solution by using (for loop)
-    String result = "";
+        // Prompt user to give a string and a number
+        Console.WriteLine("Enter number of copies: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+        
+        // Prompt user to enter a string
+        Console.WriteLine("Enter a string: ");
+        string str = Console.ReadLine();
+        
+        //Repeat the string to the number of n
+        
+        // This is the short solution by using (Concat and Repeat methods)
+        // String result = string.Concat(Enumerable.Repeat(str, n));
+        
+        // This is the longer solution by using (for loop)
+        String result = "";
 
-    for (int i = 0; i < n; i++)
-    {
-        result = result + str;
+        for (int i = 0; i < n; i++)
+        {
+            result = result + str + ". ";
+        }
+
+        return result;
     }
-
-    return result;
-}
 }
